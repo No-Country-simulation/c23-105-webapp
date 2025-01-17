b3d5e9a3bb2254f12a4ce4d5372b507130bffa15
BrunoCollazo <collazobruno2@gmail.com>
2025-01-17 01:35:19 -0300

    chore(backend): initialized spring project

---
+++ backend/src/test/java/com/nocountry/FriendlySpots/FriendlySpotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.FriendlySpots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlySpotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
